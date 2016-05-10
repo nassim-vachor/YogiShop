99755cbf4244b6d06bf16050e0108582984814cb
Djeneba KANE <kanedjene@yahoo.fr>
2016-05-10 19:56:39 +0200

    login/logoff ok

--- header.php
+++ header.php
@@ -1,34 +1,43 @@
 
 <?php
-       
+
+
+       require_once("connectdb.php");
+       $dbh = connect();
+
             // Pour tester s'il a des cookies "idPerson" et "token"
-        if (isset($_COOKIE['idPerson']) && isset($_COOKIE['token']))
+        if (isset($_COOKIE['id']) && isset($_COOKIE['token']))
         {
             // si oui on les stocke dans des variables
-            $id = $_COOKIE['idPerson'];
-            $requete = $dbh->query("SELECT token FROM Person WHERE idPerson = '$id'");
-            $row =$requete->fetch();
-
+            $id = $_COOKIE['id'];
+            $requete = $dbh->query("SELECT Token FROM Person WHERE idPerson = '$id'");
+            $row=$requete->fetch();
+ 
             //on verifie si le token est vide ou si le token recuperé est different de celle de la bd 
             // ou si son token est egal a 0 (dans ce cas, la personne est deconnectée)
-            if(!isset($_COOKIE['token']) || $_COOKIE['token'] == '0' || $row['token'] != $_COOKIE['token'])
+            if(!isset($_COOKIE['token']) || $_COOKIE['token'] == '0' || $row['Token'] != $_COOKIE['token'])
             {
+                // tu est deconnecte => button connexion
+                
+
                 include("menuOff.php");
             }
             else 
             {
-               
-                $requete = $dbh->query("SELECT idPerson FROM Admin WHERE idPerson = '$id'");
+               // la personne est connectée => button connexion
+                
+                $requete = $dbh->query("SELECT idPerson FROM Person WHERE EstAdmin = '$id'");
                 $row =$requete->fetch();
 
                 // verification du type de personne : admin ou adherent
-                if($rowCount>0){
+                // user connecte :
+                if($row>0){
               
                 include("menuOnAdmin.php");
                 }
                 else 
                 {
-                      include("menuOnUser.php");
+                     include("menuOnUser.php");
                 }
             } 
         }
